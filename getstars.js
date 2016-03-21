cb670a428d1c4a8f797bfba7bd3c2ad263555a8e
Yoshihiro Kikuchi <yknetg@gmail.com>
2016-03-21 17:50:30 +0900

    update

--- getstars.js
+++ getstars.js
@@ -5,20 +5,35 @@ var repos = require('./repos');
 var apiURL = 'https://api.github.com/repos/';
 var promises = [];
 
-for (var repo of repos) {
+if (!process.env.GHTOKEN) {
+  console.log('environment variable GHTOKEN needed.');
+  process.exit(1);
+}
+
+function makeRequest(repo) {
   var p = new Promise(function(resolve, reject) {
     request
       .get(apiURL + repo)
+      .set('User-Agent', 'superagent')
+      .set('Authorization', 'token ' + process.env.GHTOKEN)
       .end(function(err, res) {
         var body = res.body;
         resolve(body);
       });
   });
-  promises.push(p);
+
+  return p;
+}
+
+
+for (var repo of repos) {
+  p = makeRequest(repo);
+  promises.push(p)
 }
 
 Promise.all(promises).then(function(results) {
-  for (var r of results) {
+  for (var key in results) {
+    var r = results[key];
     console.log(r.full_name + ' : ' + r.stargazers_count);
   }
 });
